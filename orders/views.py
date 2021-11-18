286c91bc8ca66f556453755aca7ffca4d4a8ccaf
Eduardo López <eduardorostran@yahoo.com>
2021-11-18 15:45:04 -0600

    🚧 - working on the views

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import *
 
 # Create your views here.
 
+def login(request):
+    pass
+
+def menu(request):
+    pass
+
+def order(request):
+    pass
+
+#el deslogueo esta en las rutas de users
